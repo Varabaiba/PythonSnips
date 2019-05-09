b1ba3ef61381b844ce830939e8a48e9b13324ef5
Varabaiba <Teimuraz@sakartvelo.net>
2019-05-09 18:56:08 +0400

    Create MasterLogTemplate.py

---
+++ MasterLogTemplate.py
@@ -0,0 +1,29 @@
+import logging
+
+logger = logging.getLogger(__name__)
+#logging.basicConfig(level=logging.DEBUG)
+
+# Create handlers
+c_handler = logging.StreamHandler()
+f_handler = logging.FileHandler('file.log')
+
+
+# Create formatters and add it to handlers
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
+
+# Add handlers to the logger
+c_handler.setLevel(logging.DEBUG)
+f_handler.setLevel(logging.DEBUG)
+logger.addHandler(c_handler)
+logger.addHandler(f_handler)
+
+
+
+logger.debug('This is a debug message')
+logger.info('This is an info message')
+logger.warning('This is a warning message')
+logger.error('This is an error message')
+logger.critical('This is a critical message')
