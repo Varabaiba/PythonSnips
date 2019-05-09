8f2f40b8824fee6fa70740905f6eab7775a4f5e6
Varabaiba <Teimuraz@sakartvelo.net>
2019-05-09 21:49:11 +0400

    Create logman.py

---
+++ modules/logman.py
@@ -0,0 +1,40 @@
+#Logman Module
+#from logman import logger
+
+import logging
+
+logger = logging.getLogger(__name__)
+# Magic necessary to set minimum logging level for all handlers
+logger.setLevel(logging.DEBUG)
+# Do not use this as advised elsewhere - logging.basicConfig(level=logging.DEBUG)
+
+# Create handlers
+c_handler = logging.StreamHandler()
+f_handler = logging.FileHandler('file.log')
+
+
+# Create formatters and add it to handlers
+c_format = logging.Formatter('T:%(asctime)s - M:%(module)s - LN:%(lineno)4d - LVL:%(levelname)s - %(message)s',
+           datefmt='%d/%m/%y %H:%M:%S')
+f_format = logging.Formatter('T:%(asctime)s - M:%(module)s - LN:%(lineno)4d - LVL:%(levelname)s - %(message)s',
+           datefmt='%d/%m/%y %H:%M:%S')
+c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
+
+# Add handlers to the logger
+c_handler.setLevel(logging.DEBUG)
+f_handler.setLevel(logging.DEBUG)
+logger.addHandler(c_handler)
+# Comment this if no file logging is necessary
+logger.addHandler(f_handler)
+
+
+# logger.debug('This is a debug message')
+# logger.info('This is an info message')
+# logger.warning('This is a warning message')
+# logger.error('This is an error message')
+# logger.critical('This is a critical message')
+# logger.log(15, "This is a manual message" )
+
+#logger.log(): Manually emits a logging message with a specific log level.
+#logger.exception(): Creates an ERROR level logging message wrapping the current exception stack frame.
