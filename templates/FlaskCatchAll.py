b76547ff5255cf38c808fcadb7d90bbaf072460b
Varabaiba <Teimuraz@sakartvelo.net>
2019-08-21 13:05:06 +0400

    Update FlaskCatchAll.py

--- templates/FlaskCatchAll.py
+++ templates/FlaskCatchAll.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return 'You want path: %s' % path
+    xSubPath = path.split("/")
+    return 'You want path: %s' % xSubPath
 
 if __name__ == '__main__':
     app.run()
