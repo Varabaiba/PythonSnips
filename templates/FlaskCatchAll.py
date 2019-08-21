632a7718ccc88c4760076f9b8a4fd67c93292a1b
Varabaiba <Teimuraz@sakartvelo.net>
2019-08-21 12:58:22 +0400

    Update FlaskCatchAll.py

--- templates/FlaskCatchAll.py
+++ templates/FlaskCatchAll.py
@@ -8,3 +8,8 @@ def catch_all(path):
 
 if __name__ == '__main__':
     app.run()
+
+'''    
+if __name__ == '__main__':  
+     app.run(host='127.0.0.1', port=80, debug=True, ssl_context='adhoc')
+'''
